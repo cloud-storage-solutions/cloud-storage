18d547033b6da2e9eb4b87d24aed081d6833e52c
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-05-08 00:52:46 +0300

    Fix CLI project issues

---
+++ command-line-interface/src/main/java/org/cloud/storage/cli/App.java
@@ -0,0 +1,13 @@
+package org.cloud.storage.cli;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ command-line-interface/src/test/java/org/cloud/storage/cli/AppTest.java
@@ -0,0 +1,20 @@
+package org.cloud.storage.cli;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
