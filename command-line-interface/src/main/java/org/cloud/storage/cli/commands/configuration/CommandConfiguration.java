57ce92f48288e7cfcec0677bf26c643dc8ecc84a
Nikola-Popov <nikola.angelov.popov@gmail.com>
2018-05-12 20:43:09 +0300

    introduced lombok

--- command-line-interface/src/main/java/org/cloud/storage/cli/commands/configuration/CommandConfiguration.java
+++ command-line-interface/src/main/java/org/cloud/storage/cli/commands/configuration/CommandConfiguration.java
@@ -7,9 +7,14 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Configuration
 @ConfigurationProperties(prefix = "cloud.worker.rest")
 @PropertySource("classpath:/config/command.properties")
+@Getter
+@Setter
 public class CommandConfiguration {
 	@NotBlank
 	@Pattern(regexp = "http|https")
@@ -20,28 +25,4 @@ public class CommandConfiguration {
 
 	@NotBlank
 	private int port;
-
-	public String getScheme() {
-		return scheme;
-	}
-
-	public void setScheme(String scheme) {
-		this.scheme = scheme;
-	}
-
-	public String getHost() {
-		return host;
-	}
-
-	public void setHost(String host) {
-		this.host = host;
-	}
-
-	public int getPort() {
-		return port;
-	}
-
-	public void setPort(int port) {
-		this.port = port;
-	}
 }
